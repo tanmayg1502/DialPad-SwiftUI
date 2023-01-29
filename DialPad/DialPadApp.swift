24a8e6296f373707a91ac9e805a0d0c974297e0f
Tanmay Goel <tanmaygoel1502@gmail.com>
2023-01-29 15:44:06 +0530

    Initial Commit

---
+++ DialPad/DialPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  DialPadApp.swift
+//  DialPad
+//
+//  Created by Tanmay Goel on 29/01/23.
+//
+
+import SwiftUI
+
+@main
+struct DialPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
